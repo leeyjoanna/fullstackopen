0d9d24f063efaa860773a2d00243b55c2ddd8eb8
Joanna <leejoanna94@gmail.com>
2021-08-12 13:11:37 -0700

    updated phonebook with database

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,21 +1,30 @@
 import './App.css';
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import SearchForm from './components/SearchForm'
 import PersonForm from './components/PersonForm'
 import Display from './components/Display'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: '1'},
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: '2' },
-    { name: 'Dan Abramov', number: '12-43-234345', id: '3' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: '4' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newSearch, setNewSearch ] = useState('')
-  const [ showSearch, setShowSearch ] = useState(persons)
+  const [ showSearch, setShowSearch ] = useState([])
 
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+        setShowSearch(response.data)
+      })
+  }, [])
+
+  console.log('people', persons)
+  console.log('show', showSearch)
 
   const addPerson = (event) => {
     event.preventDefault()
